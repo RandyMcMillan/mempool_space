98d7bdfe2b5d4f4f557bd439d844ab16e5b7e2ea
randymcmillan <randymcmillan@protonmail.com>
2024-08-09 18:28:03 -0400

    src/bin/mempool-space_historical_prices.rs:intermediate

--- src/bin/mempool-space_historical_prices.rs
+++ src/bin/mempool-space_historical_prices.rs
@@ -1,20 +1,33 @@
-use std::io::Read;
-use std::time::{Instant, SystemTime};
-
 use mempool_space::blocking;
-use reqwest::Url;
-
-// use ureq::get;
-
-const URL: &str = "https://mempool.space/api/v1/historical-price";
+use std::env;
 
 fn main() {
-    let n = 1;
     {
-        let start = Instant::now();
-        let historical_price = &String::from("historical-price");
-        let res_format = format!("/v1/{}", historical_price);
-        let res = blocking(&res_format);
-        println!("blocking {:?} {:?} bytes ", start.elapsed(), res);
+        let args: Vec<String> = env::args().collect();
+        let mut currency = &String::from("");
+        let mut timestamp = String::from("");
+        if args.len() > 1 {
+            //currency first position
+            currency = &args[1];
+            if args.len() > 2 {
+                //timestamp second position
+                timestamp = args[2].clone();
+            } else {
+                //timestamp now in seconds
+                //use std::time::SystemTime;
+                //let now = format!("{:?}", SystemTime::now());
+                timestamp = format!("{:}", format!("{:?}", SystemTime::now()));
+            }
+        } else {
+            // silence is golden
+            std::process::exit(0);
+        }
+        print!("timestamp={}", timestamp);
+        use std::time::SystemTime;
+        let _res = blocking(&format!(
+            "v1/historical-price?currency={}&timestamp={}",
+            &format!("{:}", &currency),
+            &format!("{:}", &timestamp)
+        ));
     }
 }
