324ef578d9196a0f216cfd9e77fdd92836e3664d
randymcmillan <randymcmillan@protonmail.com>
2024-08-19 16:41:31 -0400

    pub fn blocking(api: &String) -> Result<&str, ascii::AsciiChar>

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,5 @@
-const API_VERSION: &str = "v1";
+/// const API_VERSION: &str = "v1";
+//
 ///  `pub fn api(option: &str, sub_string: &str) -> String`
 pub fn api(option: &str, sub_string: &str) -> String {
     use std::process::Command;
@@ -27,4 +28,35 @@ pub fn api(option: &str, sub_string: &str) -> String {
     print!("{}", result);
     result
     //}
+}
+use crate::URL;
+///  `pub fn blocking(api: &String) -> Result<&str>`
+///
+pub fn blocking(api: &String) -> Result<&str, ascii::AsciiChar> {
+    //print!("api={}", api);
+    let call = format!("{}/{}", URL, api);
+    let mut body = ureq::get(&call)
+        .call()
+        .expect("calls to blocking(api: &String) needs to include /v1/<api_endpoint> in some cases.")
+        .into_reader();
+    let mut buf = Vec::new();
+    body.read_to_end(&mut buf).unwrap();
+    if !api.ends_with("raw") {
+        //print!("!api.ends_with raw");
+        let text = match std::str::from_utf8(&buf) {
+            Ok(s) => s,
+            Err(_) => panic!("Invalid ASCII data"),
+        };
+        print!("{}", text);
+    } else {
+        if api.ends_with("raw") {
+            //print!("api.ends_with raw");
+            print!("{:?}", &buf);
+        }
+        if api.ends_with("something_else") {
+            //print!("api.ends_with something_else");
+            print!("{:?}", &buf);
+        }
+    }
+    Ok(api)
 }
