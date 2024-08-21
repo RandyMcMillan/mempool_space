c47b6b3e6fd42db6edd22d24efb9f915b9a3571b
randymcmillan <randymcmillan@protonmail.com>
2024-08-21 13:20:17 -0400

    src/bin/mempool-space_lighting_channel_geodata_for_node.rs

--- src/bin/mempool-space_lighting_channel_geodata_for_node.rs
+++ src/bin/mempool-space_lighting_channel_geodata_for_node.rs
@@ -1,16 +1,16 @@
-use mempool_space::blocking::blocking;
+use mempool_space::api::blocking;
 use std::env;
 
 fn main() {
     {
         let args: Vec<String> = env::args().collect();
-        let mut address = &String::from("");
         if args.len() > 1 {
-            address = &args[1];
+            let mut pubkey = &String::from("");
+            pubkey = &args[1];
+            let _res = blocking(&format!("v1/lightning/channels-geo/{}", &pubkey));
         } else {
             // silence is golden
             std::process::exit(0);
         }
-        let _res = blocking(&format!("/address/{}", &address));
     }
 }
