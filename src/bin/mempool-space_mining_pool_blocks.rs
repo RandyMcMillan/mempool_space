a57a0bd3481a1a28ebd9c5f928783bec70cbf735
randymcmillan <randymcmillan@protonmail.com>
2024-08-21 13:08:46 -0400

    src/bin/mempool-space_mining_pool_blocks.rs

--- src/bin/mempool-space_mining_pool_blocks.rs
+++ src/bin/mempool-space_mining_pool_blocks.rs
@@ -1,16 +1,22 @@
-use mempool_space::blocking::blocking;
+use mempool_space::api::blocking;
 use std::env;
 
 fn main() {
     {
         let args: Vec<String> = env::args().collect();
-        let mut address = &String::from("");
-        if args.len() > 1 {
-            address = &args[1];
+        if args.len() == 2 {
+            let mut slug = &String::from("");
+            slug = &args[1];
+            let _res = blocking(&format!("v1/mining/pool/{}", &slug));
+        } else if args.len() == 3 {
+            let mut slug = &String::from("");
+            let mut blockheight = &String::from("");
+            slug = &args[1];
+            blockheight = &args[2];
+            let _res = blocking(&format!("v1/mining/pool/{}/blocks/{}", &slug, &blockheight));
         } else {
             // silence is golden
             std::process::exit(0);
         }
-        let _res = blocking(&format!("/address/{}", &address));
     }
 }
