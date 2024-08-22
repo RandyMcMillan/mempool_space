fb9e87c764510e701c1d7f63e214771c8cba1d8b
randymcmillan <randymcmillan@protonmail.com>
2024-08-22 15:17:18 -0400

    src/bin/mempool-space_validate_address.rs

--- src/bin/mempool-space_validate_address.rs
+++ src/bin/mempool-space_validate_address.rs
@@ -4,13 +4,13 @@ use std::env;
 fn main() {
     {
         let args: Vec<String> = env::args().collect();
-        let mut address = &String::from("");
         if args.len() > 1 {
+            let mut address = &String::from("");
             address = &args[1];
+            let _res = blocking(&format!("v1/validate-address/{}", &address));
         } else {
             // silence is golden
             std::process::exit(0);
         }
-        let _res = blocking(&format!("v1/validate-address/{}", &address));
     }
 }
