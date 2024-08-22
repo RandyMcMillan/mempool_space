83082e76b46a1eca5dfcccd52470ff90caffac2b
randymcmillan <randymcmillan@protonmail.com>
2024-08-21 22:29:28 -0400

    src/bin/mempool-space_blocks_timestamp.rs:clean up

--- src/bin/mempool-space_blocks_timestamp.rs
+++ src/bin/mempool-space_blocks_timestamp.rs
@@ -3,15 +3,10 @@ use std::env;
 use std::time::SystemTime;
 
 fn main() {
-    // match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-    //     Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
-    //     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    // };
     let now_secs = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
     {
         let args: Vec<String> = env::args().collect();
         if args.len() == 1 {
-            //let now_secs = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
             let _res = blocking(&format!("v1/mining/blocks/timestamp/{:?}", now_secs.unwrap().as_secs()));
         } else if args.len() == 2 {
             let mut utc_secs = &String::from("");
