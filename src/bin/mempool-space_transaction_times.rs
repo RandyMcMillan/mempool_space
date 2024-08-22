b6a23760518b238713497813467044213b571ade
randymcmillan <randymcmillan@protonmail.com>
2024-08-22 15:25:46 -0400

    src/bin/mempool-space_transaction_times.rs

--- src/bin/mempool-space_transaction_times.rs
+++ src/bin/mempool-space_transaction_times.rs
@@ -1,16 +1,25 @@
-use mempool_space::blocking::blocking;
+use mempool_space::api::blocking;
 use std::env;
+use std::time::SystemTime;
 
 fn main() {
-    {
-        let args: Vec<String> = env::args().collect();
-        let mut address = &String::from("");
-        if args.len() > 1 {
-            address = &args[1];
-        } else {
-            // silence is golden
-            std::process::exit(0);
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        //v1/transaction-times?
+        let mut query = String::from("v1/transaction-times?");
+        let mut count = 0;
+        for arg in args.clone() {
+            if count > 0 {
+                //print!("txId[]={}\n",arg);
+                query.push_str(&("txId[]=".to_owned() + &arg));
+                query.push_str(&("&".to_owned()));
+            }
+            count = count + 1;
         }
-        let _res = blocking(&format!("/address/{}", &address));
+        //print!("{}", query);
+        let _res = blocking(&format!("{}", &query));
+    } else {
+        // silence is golden
+        std::process::exit(0);
     }
 }
