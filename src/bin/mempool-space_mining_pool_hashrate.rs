70ea6e057ac7cf26dcc81aa31b22c5c1195dbfca
randymcmillan <randymcmillan@protonmail.com>
2024-08-22 16:11:31 -0400

    src/bin/mempool-space_mining_pool_hashrate.rs

--- src/bin/mempool-space_mining_pool_hashrate.rs
+++ src/bin/mempool-space_mining_pool_hashrate.rs
@@ -5,9 +5,9 @@ fn main() {
     {
         let args: Vec<String> = env::args().collect();
         if args.len() > 1 {
-        let mut slug = &String::from("");
+            let mut slug = &String::from("");
             slug = &args[1];
-        let _res = blocking(&format!("v1/mining/pool/{}/hashrate", &slug));
+            let _res = blocking(&format!("v1/mining/pool/{}/hashrate", &slug));
         } else {
             // silence is golden
             std::process::exit(0);
