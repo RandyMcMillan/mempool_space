894a041323a799ec257758c406c439d8b29bfac9
randymcmillan <randymcmillan@protonmail.com>
2024-08-23 16:11:05 -0400

    src/bin/mempool-space_lightning_channels_from_node_pubkey.rs

--- src/bin/mempool-space_lightning_channels_from_node_pubkey.rs
+++ src/bin/mempool-space_lightning_channels_from_node_pubkey.rs
@@ -6,7 +6,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
         let public_key = &args[1];
-        let _res = blocking(&format!("v1/lightning/channels?public_key={}&status=active", &public_key));
+        let _res = blocking(&format!(
+            "v1/lightning/channels?public_key={}&status=active",
+            &public_key
+        ));
     } else if args.len() == 3 {
         let public_key = &args[1];
         let status = &args[2];
