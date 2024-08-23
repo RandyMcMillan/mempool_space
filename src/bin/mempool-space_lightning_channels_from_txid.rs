88a823363e8a37ca41e714f674456db04fbfaea5
randymcmillan <randymcmillan@protonmail.com>
2024-08-23 12:24:49 -0400

    src/bin/mempool-space_lightning_channels_from_txid.rs:docs

--- src/bin/mempool-space_lightning_channels_from_txid.rs
+++ src/bin/mempool-space_lightning_channels_from_txid.rs
@@ -2,11 +2,11 @@ use mempool_space::api::blocking;
 use std::env;
 use std::time::SystemTime;
 
-//REF: v1/lightning/channels/txids?txId[]=c3173549f502ede6440d5c48ea74af5607d88484c7a912bbef73d430049f8af4&txId[]=d78f0b41a263af3df91fa4171cc2f60c40196aaf8f4bde5d1c8ff4474cfe753b
-//
+// COMMAND EXAMPLE
+// mempool-space_lighting_channels_from_txid c3173549f502ede6440d5c48ea74af5607d88484c7a912bbef73d430049f8af4 d78f0b41a263af3df91fa4171cc2f60c40196aaf8f4bde5d1c8ff4474cfe753b
 
-//REF: mempool-space_lighting_channels_from_txid c3173549f502ede6440d5c48ea74af5607d88484c7a912bbef73d430049f8af4 d78f0b41a263af3df91fa4171cc2f60c40196aaf8f4bde5d1c8ff4474cfe753b
-
+/// [GET /api/v1/lightning/channels/txids?txId[]=:txid](https://mempool.space/api/v1/lightning/channels/txids?txId[]=c3173549f502ede6440d5c48ea74af5607d88484c7a912bbef73d430049f8af4&txId[]=d78f0b41a263af3df91fa4171cc2f60c40196aaf8f4bde5d1c8ff4474cfe753b)
+///
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
