093a1e62511014c98a26494b9402aed6cf00c60f
randymcmillan <randymcmillan@protonmail.com>
2024-08-23 11:25:13 -0400

    cargo-dist:update:v0.21.1

--- cargo.mk
+++ cargo.mk
@@ -90,7 +90,7 @@ cargo-dist:### 	cargo-dist -h
 	cargo dist -h
 cargo-dist-build:### 	cargo-dist-build
 	RUSTFLAGS="--cfg tokio_unstable" cargo dist build
-cargo-dist-manifest-global:### 	cargo dist manifest --artifacts=all
-	cargo dist manifest --artifacts=all
+cargo-dist-manifest-global:### 	cargo dist manifest --artifacts=global
+	cargo dist manifest --artifacts=global
 # vim: set noexpandtab:
 # vim: set setfiletype make
