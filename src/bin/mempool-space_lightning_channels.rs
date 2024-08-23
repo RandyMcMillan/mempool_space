0ff74c9c53583872254e5548e9eba1bd26d106c1
randymcmillan <randymcmillan@protonmail.com>
2024-08-23 12:10:25 -0400

    src/bin/mempool-space_lightning_channels.rs

--- src/bin/mempool-space_lightning_channels.rs
+++ src/bin/mempool-space_lightning_channels.rs
@@ -5,8 +5,7 @@ fn main() {
     {
         let args: Vec<String> = env::args().collect();
         if args.len() > 1 {
-            let mut channel_id = &String::from("");
-            channel_id = &args[1];
+            let channel_id = &args[1];
             let _res = blocking(&format!("v1/lightning/channels/{}", &channel_id));
         } else {
             // silence is golden
