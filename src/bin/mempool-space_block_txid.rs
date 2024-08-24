94adf8290a4065d1cd300ec86836741e63ef253d
randymcmillan <randymcmillan@protonmail.com>
2024-08-24 12:26:50 -0400

    src/bin/mempool-space_block_txid.rs

--- src/bin/mempool-space_block_txid.rs
+++ src/bin/mempool-space_block_txid.rs
@@ -5,10 +5,8 @@ fn main() {
     {
         let args: Vec<String> = env::args().collect();
         if args.len() == 3 {
-            let mut block = &String::from("");
-            let mut index = &String::from("");
-            block = &args[1];
-            index = &args[2];
+            let block = &args[1];
+            let index = &args[2];
             let _res = blocking(&format!("block/{}/txid/{}", &block, &index));
         } else {
             // silence is golden
