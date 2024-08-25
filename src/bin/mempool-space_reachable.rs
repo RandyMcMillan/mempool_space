95cc545a75d72004835dbcbc4a6ef85d48d88e1a
randymcmillan <randymcmillan@protonmail.com>
2024-08-25 19:07:54 -0400

    src/bin/mempool-space_reachable.rs:blocking and api calls

--- src/bin/mempool-space_reachable.rs
+++ src/bin/mempool-space_reachable.rs
@@ -1,17 +1,17 @@
 use std::time::{Instant};
 
-use mempool_space::api::blocking;
+use mempool_space::api::{api,blocking};
 
 // use ureq::get;
-
-const URL: &str = "https://mempool.space/api/blocks/tip/height";
-
 fn main() {
-    let n = 1;
     {
         let start = Instant::now();
-        let blocks_tip_height = String::from("/blocks/tip/height");
+        let blocks_tip_height = String::from("blocks/tip/height");
         let res = blocking(&blocks_tip_height);
-        println!("blocking {:?} {:?} bytes", start.elapsed(), res);
+        println!("\nblocking {:?} {:?} bytes", start.elapsed(), res);
+        let start = Instant::now();
+        let blocks_tip_height = String::from("blocks_tip_height");
+        let res = api(&blocks_tip_height, &"");
+        println!("\napi {:?} {:?} bytes", start.elapsed(), res);
     }
 }
