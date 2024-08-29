17152d76363916c3563aa229a80f7726d06b63ab
randymcmillan <randymcmillan@protonmail.com>
2024-08-29 19:06:04 -0400

    src/api.rs:cleanup

--- src/api.rs
+++ src/api.rs
@@ -4,11 +4,6 @@
 pub fn api(option: &str, sub_string: &str) -> String {
     use std::process::Command;
 
-    //if sub_string == "v1" {
-    //    print!("TODO: support --version v1 api versioning.");
-    //} else if sub_string == "v2" {
-    //    print!("TODO: support --version v2 api versioning.");
-    //} else {
     let output = if cfg!(target_os = "windows") {
         Command::new(format!("mempool-space_{}", option))
             .args(["/C", sub_string])
@@ -17,7 +12,6 @@ pub fn api(option: &str, sub_string: &str) -> String {
     } else {
         Command::new(format!("mempool-space_{}", option))
             .arg(sub_string)
-            //.arg("echo hello")
             .output()
             .expect("failed to execute process")
     };
@@ -25,9 +19,7 @@ pub fn api(option: &str, sub_string: &str) -> String {
     let result = String::from_utf8(output.stdout)
         .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
         .unwrap();
-    print!("{}", result);
     result
-    //}
 }
 use crate::URL;
 /// pub fn blocking(api: &String) -> Result<&str, ascii::AsciiChar>
