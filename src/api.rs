4d66821be66c06ba52743a82cefedce8246ffda9
randymcmillan <randymcmillan@protonmail.com>
2024-08-29 19:26:30 -0400

    v0.0.40

--- src/api.rs
+++ src/api.rs
@@ -19,6 +19,7 @@ pub fn api(option: &str, sub_string: &str) -> String {
     let result = String::from_utf8(output.stdout)
         .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
         .unwrap();
+    println!("{}", result);
     result
 }
 use crate::URL;
